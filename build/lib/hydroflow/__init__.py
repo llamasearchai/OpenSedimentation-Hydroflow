71eb4d7579a2408984214572fb072122cc0b6ca1
Nik Jois <nikjois@users.noreply.github.com>
2025-09-12 18:56:39 -0700

    chore(repo): untrack venv/build/dist/egg-info

--- build/lib/hydroflow/__init__.py
+++
@@ -1,9 +0,0 @@
-"""HydroFlow - Advanced Bathymetric Analysis and Sediment Transport Management System."""
-
-from .__version__ import __version__
-from .core.config import Config
-from .core.exceptions import HydroFlowError
-
-__all__ = ["__version__", "Config", "HydroFlowError"]
-
-
